8ca615d0bc86f5fe9722244c0d59b76cf0477c7d
Abdvo <james10soumah@gmail.com>
2022-06-29 12:13:18 +0200

    feat: change package name

--- app/src/main/java/com/abdvo/listas/MainActivity.kt
+++ app/src/main/java/com/abdvo/listas/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.jocnunez.listas
+package com.abdvo.listas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jocnunez.listas.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
